4f21cfb74457884f2827950c92eb9c52d42da982
Simarenas21 <tankevicius.simas21@gmail.com>
2025-08-18 22:30:41 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Cube-shop-3/', 
+  base: '/Cube-shop-3/', // labai svarbu GH Pages
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': 'http://localhost:3000'
-    }
-  },
-  build: {
-    outDir: 'dist'
-  }
+  server: { port: 5173, proxy: { '/api': 'http://localhost:3000' } },
+  build: { outDir: 'dist' }
 })
